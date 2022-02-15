45211e26ebfc35844b4a98ff46cc75981af96b83
Harry <hrgus3@gmail.com>
2022-02-15 03:46:11 -0500

    Assignment Complete

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [newItem, setNewItem] = useState({
+    name: "",
+    category: "produce",
+  });
+
+  const onItemChange = (e) => {
+    setNewItem({
+      ...newItem,
+      [e.target.name]: e.target.value,
+     
+    });
+    
+  };
+
+  const secondNewItem  = {
+    id: uuid(),
+    name: newItem.name,
+    category: newItem.category,
+  };
+
   return (
-    <form className="NewItem">
+    <form 
+      className="NewItem"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onItemFormSubmit(secondNewItem)
+        setNewItem({
+          name: "",
+          category: "produce",
+        });
+      }}
+        
+      >
       <label>
-        Name:
-        <input type="text" name="name" />
+        Name:  
+        <input 
+          onChange={onItemChange}
+          value={newItem.name} 
+          type="text" 
+          name="name"
+          />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+          onChange={onItemChange}
+          value={newItem.category}
+          name="category"
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,23 +5,42 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+  const [listItem, setListItem] = useState(items);
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  const onItemFormSubmit = (newItem) => {
+    setListItem([...listItem, newItem]);
+  };
+
+  const onSearchChange  = (e) => {
+    setSearch(e.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleCategoryChange(e) {
+    setSelectedCategory(e.target.value);
+  };
+
+  const itemsToDisplay = listItem.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const filteredItemsToDisplay = itemsToDisplay.filter((item) => {
+   return item.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}
+      />
+      <Filter 
+        onCategoryChange={handleCategoryChange} 
+        onSearchChange={onSearchChange}
+        search={search}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
